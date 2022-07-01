73786ac8b8b5a9c4090b34cb39937b337b81b561
gauravgjadhav <92154289+gauravgjadhav@users.noreply.github.com>
2022-07-01 22:50:00 +0530

    Add files via upload

---
+++ Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IheritanceExampleApp.Model
+{
+    class Car : Vehicle  
+    {
+        public string modelName = "Mustang";  
+    }
+}
---
+++ Vehical.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IheritanceExampleApp.Model
+{
+    class Vehicle  // Base class
+    {
+        public string brand = "Ford";  // Vehicle field
+        public void honk()             // Vehicle method 
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+}
